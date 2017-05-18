2e7c2385b54c42d2af1c6a991c49959df102c814
JeanCarlo14 <jkvargas_14@hotmail.com>
2017-05-18 12:49:00 -0600

    Proyecto Moviles
    Proyecto Moviles

---
+++ app/src/main/java/com/example/hilla/geomapcr/MainMenu.java
@@ -0,0 +1,241 @@
+package com.example.hilla.geomapcr;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        Mensaje("Bienvenidos a GeoMapCR");
+        ReproducirAudio();
+        OnclickDelButton(R.id.btnTest);
+        OnclickDelButton(R.id.btnMapa);
+        OnclickDelButton(R.id.btnRelax);
+        OnclickDelButton(R.id.btnAyuda);
+        OnclickDelButton(R.id.btnJugar);
+
+        Button boton = (Button) findViewById(R.id.btnJugar);
+        Button boton2 = (Button) findViewById(R.id.btnMapa);
+        registerForContextMenu(boton);
+        registerForContextMenu(boton2);
+
+
+    }//fin oncreate
+    MediaPlayer misonido;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void ReproducirAudio(){
+        misonido = MediaPlayer.create(this, R.raw.uno);
+        misonido.start();
+
+    }
+
+    public void PararReproducirAudio(){
+        misonido.stop();
+
+    }
+
+    public void Mensaje(String msg){getSupportActionBar().setTitle(msg);};
+
+    public void OnclickDelButton(int ref) {
+        // Ejemplo  OnclickDelButton(R.id.MiButton);
+        // 1 Doy referencia al Button
+        View view =findViewById(ref);
+        Button miButton = (Button) view;
+        //  final String msg = miButton.getText().toString();
+        // 2.  Programar el evento onclick
+        miButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // if(msg.equals("Texto")){Mensaje("Texto en el botón ");};
+                switch (v.getId()) {
+                    case R.id.btnJugar:
+                        openContextMenu(findViewById(R.id.btnJugar));
+                        break;
+
+                    case R.id.btnTest:
+                        Intent intento = new Intent(getApplicationContext(), Jugar.class);
+                        intento.putExtra("tipo", 6);
+                        startActivity(intento);
+                        break;
+
+                    case R.id.btnMapa:
+                        openContextMenu(findViewById(R.id.btnMapa));
+                        break;
+
+                    case R.id.btnRelax:
+                        Intent intento3 = new Intent(getApplicationContext(), Jugar.class);
+                        intento3.putExtra("tipo", 7);
+                        startActivity(intento3);
+                        break;
+
+                    case R.id.btnAyuda:
+                        Mensaje("Ayuda");
+
+                        Intent intento2 = new Intent(getApplicationContext(), Ayuda.class);
+                        startActivity(intento2);
+
+                        break;
+                    default:break; }// fin de casos
+            }// fin del onclick
+        });
+    }// fin de OnclickDelButton
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        switch (v.getId()) {
+            case R.id.btnJugar:
+                MenuInflater infla =getMenuInflater();
+                infla.inflate(R.menu.mimenu, menu);
+                break;
+            case R.id.btnMapa:
+                MenuInflater infla2 =getMenuInflater();
+                infla2.inflate(R.menu.menumapa, menu);
+                break;
+
+            default: /* Mensaje("No clasificado");*/ break;
+        }
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        int opcionseleccionada = item.getItemId();
+        Intent intento = new Intent(getApplicationContext(), Jugar.class);
+        Intent intento2 = new Intent(getApplicationContext(), MapsActivity.class);
+
+        switch (opcionseleccionada) {
+            case R.id.item1:
+                Mensaje("Ríos");
+                intento.putExtra("tipo", 3);
+                startActivity(intento);
+                PararReproducirAudio();
+                break;
+            case R.id.item2:
+                Mensaje("Cordilleras y Cerros");
+                intento.putExtra("tipo", 4);
+                startActivity(intento);
+                PararReproducirAudio();
+                break;
+            case R.id.item3:
+                Mensaje("Volcanes");
+                intento.putExtra("tipo", 1);
+                startActivity(intento);
+                PararReproducirAudio();
+                break;
+            case R.id.item4:
+                intento.putExtra("tipo", 2);
+                startActivity(intento);
+                PararReproducirAudio();
+                break;
+            case R.id.item5:
+                Mensaje("Parques Nacionales");
+                intento.putExtra("tipo", 5);
+                startActivity(intento);
+                PararReproducirAudio();
+                break;
+            case R.id.item6:
+                intento2.putExtra("tipo", 3);
+                startActivity(intento2);
+                PararReproducirAudio();
+                break;
+            case R.id.item7:
+                intento2.putExtra("tipo", 4);
+                startActivity(intento2);
+                PararReproducirAudio();
+                break;
+            case R.id.item8:
+                intento2.putExtra("tipo", 1);
+                startActivity(intento2);
+                PararReproducirAudio();
+                break;
+            case R.id.item9:
+                intento2.putExtra("tipo", 2);
+                startActivity(intento2);
+                PararReproducirAudio();
+                break;
+            case R.id.item10:
+                intento2.putExtra("tipo", 5);
+                startActivity(intento2);
+                PararReproducirAudio();
+                break;
+            case R.id.item11:
+                intento2.putExtra("tipo", 7);
+                startActivity(intento2);
+                PararReproducirAudio();
+                break;
+
+
+            default:  Mensaje("No clasificado"); break;
+        }
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.item12:
+                Intent intento3 = new Intent(getApplicationContext(), Info_Integrantes.class);
+                startActivity(intento3);
+                PararReproducirAudio();
+                break;
+            default:  /*Mensaje("No clasificado");*/ break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        ReproducirAudio();
+        // Mensaje2("Pase por OnStart");
+    };
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        //  Mensaje2("Pase por onRestart");
+    };
+    @Override
+    protected void onResume(){
+        super.onResume();
+        // Mensaje2("Pase por onResume");
+    };
+    @Override
+    protected void onPause(){
+        super.onPause();
+        //  Mensaje2("Pase por onPause");
+        PararReproducirAudio();
+    };
+    @Override
+    protected void onStop(){
+        super.onStop();
+    };
+    //estoy tratando de que se haga un cambio en el git hub
+}//fin
+
+
+
+
+
---
+++ app/src/main/java/com/example/hilla/geomapcr/MapsActivity.java
@@ -0,0 +1,271 @@
+package com.example.hilla.geomapcr;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private Marker marcador;
+    double lat = 0.0;
+    double lng = 0.0;
+    private static String url_tipo_ubicacion = "http://geomapcr.atwebpages.com/get_tipo_ubicaciones.php";
+    private static String url_all_ubicacion = "http://geomapcr.atwebpages.com/get_all_ubicaciones.php";
+    // Creating JSON Parser object
+    JSONParser jParser = new JSONParser();
+    JSONArray ubicaciones = null;
+    private ProgressDialog pDialog;
+    private int tipo = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+        ubicaciones = new JSONArray();
+        Intent callingIntent = getIntent();
+        tipo = callingIntent.getIntExtra("tipo", 1);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        new LoadAllProducts().execute(); // ver en que momento usar
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        miUbicacion();
+        // Add a marker in Sydney and move the camera
+        /*LatLng sydney = new LatLng(9.9961661, -84.1196966999999);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+
+        LatLng sydney3 = new LatLng(10.0009657, -84.11567230000000);
+        mMap.addMarker(new MarkerOptions().position(sydney3).title("p"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+        /*
+        *  gim(int lat, int lon){
+        *      LatLng sydney3 = new LatLng(lat, lon);
+        mMap.addMarker(new MarkerOptions().position(sydney3).title("p"));
+        *
+        *  }
+        *
+        * */
+    }
+
+    public void locGim(double lat, double lon, String nombre, int tipo){
+     /*   Volcanes          1
+        LLanuras		  2
+        Rios			  3
+        Cordilleras		  4
+        Parque Nacional   5*/
+        LatLng gimnasio = new LatLng(lat, lon);
+        switch (tipo) {
+            case 1 :    mMap.addMarker(new MarkerOptions().position(gimnasio).title(nombre).icon(BitmapDescriptorFactory.fromResource(R.drawable.volcanes)));
+           break;
+            case 2 :    mMap.addMarker(new MarkerOptions().position(gimnasio).title(nombre).icon(BitmapDescriptorFactory.fromResource(R.drawable.ullanura)));
+                break;
+            case 3 :    mMap.addMarker(new MarkerOptions().position(gimnasio).title(nombre).icon(BitmapDescriptorFactory.fromResource(R.drawable.urio)));
+                break;
+            case 4 :    mMap.addMarker(new MarkerOptions().position(gimnasio).title(nombre).icon(BitmapDescriptorFactory.fromResource(R.drawable.ucordillera)));
+                break;
+            case 5 :    mMap.addMarker(new MarkerOptions().position(gimnasio).title(nombre).icon(BitmapDescriptorFactory.fromResource(R.drawable.uparque)));
+                break;
+            default: break;
+        }
+
+
+    }
+
+
+
+    private void cargarUbicaciones(){
+
+        for(int i=0; i<ubicaciones.length();i++){
+            try {
+                JSONObject c = ubicaciones.getJSONObject(i);
+                locGim(c.getDouble("latitud"),c.getDouble("longitud"),c.getString("nombre"),c.getInt("tipo"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    private void agregarMarcador(double lat, double lng) {
+        LatLng coordenadas = new LatLng(lat, lng);
+        CameraUpdate miUbicacion = CameraUpdateFactory.newLatLngZoom(coordenadas, 9);
+        if (marcador != null) marcador.remove();
+        marcador = mMap.addMarker(new MarkerOptions()
+                .position(coordenadas)
+                .title("Posición")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.pos)));
+        mMap.animateCamera(miUbicacion);
+    }
+
+    private void actualizarUbicacion(Location location) {
+        if (location != null) {
+            lat = location.getLatitude();
+            lng = location.getLongitude();
+            agregarMarcador(lat, lng);
+        }
+
+    }
+
+    LocationListener locListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            actualizarUbicacion(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void miUbicacion() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        actualizarUbicacion(location);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,0,locListener);
+    }
+
+    class LoadAllProducts extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MapsActivity.this);
+            pDialog.setMessage("Cargando ubicaciones. Por favor espere...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        int success = 0;
+
+        /**
+         * getting un usuario from url
+         */
+        protected String doInBackground(String... args) {
+
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("tipo", tipo + ""));
+
+
+
+            // getting JSON string from URL
+            JSONObject json = null;
+            if(tipo!=7) {
+                json = jParser.makeHttpRequest(url_tipo_ubicacion, "GET", params);
+            }
+            else{
+                json = jParser.makeHttpRequest(url_all_ubicacion,"GET",params);
+            }
+           try {
+                // Checking for SUCCESS TAG
+                success = json.getInt("success");
+
+                if (success == 1) {
+                    try {
+                        // Preguntas found
+                        // Getting Array of preguntas
+                        ubicaciones = json.getJSONArray("ubicaciones");
+
+
+                    } catch (JSONException e1) {
+                        e1.printStackTrace();
+                    }
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+
+                    if (success == 1) {
+                        cargarUbicaciones();
+                        // Colocar si trabajo algo
+                    } else {
+                        // Colocar si no
+                    }
+
+
+                }
+            });
+
+        }
+
+
+    } // Fin LoadallProducts
+
+
+
+
+}
