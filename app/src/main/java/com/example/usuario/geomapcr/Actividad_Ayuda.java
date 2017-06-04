4c4c5da21d315e28a9d9cd5bcb493f50f5315ad3
JeanCarlo14 <jkvargas_14@hotmail.com>
2017-06-03 23:38:02 -0600

    Cambios link
    Cambios link

--- app/src/main/java/com/example/usuario/geomapcr/Actividad_Ayuda.java
+++ app/src/main/java/com/example/usuario/geomapcr/Actividad_Ayuda.java
@@ -1,8 +1,12 @@
 package com.example.usuario.geomapcr;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class Actividad_Ayuda extends AppCompatActivity {
 
@@ -12,6 +16,20 @@ public class Actividad_Ayuda extends AppCompatActivity {
         setContentView(R.layout.activity_actividad__ayuda);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // alambramos el FloatingActionButton
+        FloatingActionButton MiFloatingActionButton = (FloatingActionButton) findViewById(R.id.link_tutorial);
+        //Programamos el evento onclick
+        MiFloatingActionButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                Uri uri = Uri.parse("https://www.youtube.com/watch?v=Pbo7_Q5nS9Y");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+
+        });
+
 
     }
 
